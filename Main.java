8be8c0d2ff3465122d625b90cb38d08ad9a09388
Ilya <127449810+sumoomus@users.noreply.github.com>
2023-03-12 23:41:17 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double n = 1.3;
+        int result = (int)(n + 1);
+
+        System.out.println("Rounded number:" + result);
+    }
+}
+
+
